d7afb8b425bcb6978ec29c733b88f9bf6a659597
wangwei83 <wangwei83@cuit.edu.cn>
2024-07-15 22:21:13 +0800

    今天这个方法没有正确，让他自己在试

---
+++ 三角形阵列3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;	
+int main() {
+	int a[1],b[3],c[5], d[3], e[1],i=0,f,g,h;
+	cin>>c[2];
+	f = 0;
+	h = 0;
+	g = 0;
+	g  = h + 1;
+	i = h + 3;
+	i=f+1;
+	a[0] = h;
+	b[0] = h;
+	b[1] = g;
+	b[2] = b[0];
+	c[0] = h;
+	c[1] = g;
+	c[2] = i;
+	c[3] = c[1];
+	c[4] = c[0];
+	d[0] = h;
+	d[1] = g;
+	d[2] = h;
+	e[0] = h;
+	cout << a[1];
+	cout << b[3];
+	cout << c[5];
+	cout << d[3];
+	cout << e[1];
+
+
+
+
+}
